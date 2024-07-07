484688b1557bb762cdb7a0eb2a7070fd78978484
Arunvasuacchu <arunvasur@gmail.com>
2024-07-07 11:28:19 +0900

    project1 completed

---
+++ 1/index.js
@@ -0,0 +1,19 @@
+const body = document.getElementsByTagName('body')[0]
+
+// body.style.backgroundColor = 'red'
+
+function setColor(name) {
+    body.style.backgroundColor = name
+}
+
+function randomColor() {
+    // R G B --> 0 - 255.
+    const red = Math.round(Math.random() * 255)
+    const Blue = Math.round(Math.random() * 255)
+    const Green = Math.round(Math.random() * 255)
+
+    const color = `rgb(${red}, ${Blue}, ${Green})`
+    body.style.backgroundColor = color
+}
+
+randomColor()
