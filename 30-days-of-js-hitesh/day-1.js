1020835eb4ed3a6efe556609490f5240939041d6
Arunvasuacchu <arunvasur@gmail.com>
2024-07-13 09:25:51 +0900

    task 4 completed

---
+++ 30-days-of-js-hitesh/day-1.js
@@ -0,0 +1,32 @@
+// Activity 1 --> variable declaration.
+// task 1:
+var number = 340
+console.log(number)
+
+// task 2:
+let string = 'Arun'
+console.log(string)
+
+// task 3:
+const boolean = true
+console.log(boolean)
+
+// task 4
+
+const num = 23
+console.log(typeof num)
+const str = 'Arun'
+console.log(typeof str)
+const bool = false
+console.log(typeof bool)
+
+const person = new Object()
+person.name = 'Arun'
+person.age = 21
+person.country = 'india'
+console.log(typeOf(person))
+
+const arr = [1, 2, 3, 4, 54]
+console.log(typeof arr)
+
+// task 5
