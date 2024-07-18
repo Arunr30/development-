15a5dabc4294de587c4bbc51663bbc8b6fca396b
Arunvasuacchu <arunvasur@gmail.com>
2024-07-18 19:16:21 +0900

    day-6 done

---
+++ 30-days-of-js-hitesh/day-6/arrays.js
@@ -0,0 +1,62 @@
+// const myArray = [1, 2, 3, 4, 5]
+// console.log(myArray) // printing array in console
+
+// console.log(myArray.at(0)) // accessing first element
+
+// console.log(myArray.toString())
+
+// console.log(myArray.pop())
+
+// let len = myArray.push(4) // pushing new element
+// console.log(len)
+
+// let jo = myArray.join('*') // joining new element
+// console.log(jo)
+
+// let n = myArray.shift() // removing 1st element
+// console.log(n)
+// console.log(myArray)
+// let n = myArray.unshift(6)
+// console.log(n)
+// console.log(myArray)
+
+// let double = myArray.map((elem) => {
+//     return elem * 2
+// })
+// console.log(double)
+
+// let fil = myArray.filter((element) => {
+//     return element % 2 === 0
+// })
+// console.log(fil)
+
+// let red = myArray.reduce((acc, curr) => {
+//     return acc + curr
+// }, 0)
+// console.log(red)
+
+// for (let i = 0; i < myArray.length; i++) {
+//     console.log(myArray[i])
+// }
+
+// for (let n of myArray) {
+//     console.log(n)
+// }
+
+const arr = [
+    [1, 2],
+    [3, 4],
+]
+
+// let sum = 0
+
+// arr.forEach((row) => {
+//     row.forEach((element) => {
+//         sum += element
+//     })
+// })
+
+// console.log(sum)
+
+console.log(arr[0][0])
+console.log(arr[1][0])
