afe5dc1a1e26356fde09352f03432d6ae61a16ed
Arunvasuacchu <arunvasur@gmail.com>
2024-07-20 20:16:14 +0900

    done

---
+++ 30-days-of-js-hitesh/day-8/index.js
@@ -0,0 +1,17 @@
+// const userName = 'Arun'
+// const age = 21
+// console.log(`Hello My Name is ${userName} and my Age is ${age}`)
+
+// const line = `firstLine
+// secondline`
+
+// console.log(line)
+
+// array destructoring
+
+// let greet = 'Hello, World'
+// let words = greet.split(' ')
+// let [first, second] = words
+
+// console.log(first)
+// console.log(second)
---
+++ 30-days-of-js-hitesh/day-8/obj.js
@@ -0,0 +1,13 @@
+let obj = {
+    username: 'Arun',
+    age: 21,
+    salary: '5cr',
+}
+
+// Destructure the object
+let { username, age, salary } = obj
+
+// Output the results
+console.log(username) // Outputs: 'Arun'
+console.log(age) // Outputs: 21
+console.log(salary) // Outputs: '5cr'
