b258802dc9b6ec7f2c8ceccd410e08da8cfeddbe
Arunvasuacchu <arunvasur@gmail.com>
2024-07-22 19:56:18 +0900

    partially comple

--- 30-days-of-js-hitesh/day-10/index.js
+++ 30-days-of-js-hitesh/day-10/index.js
@@ -1,7 +1,32 @@
-const button = document.getElementById('button')
+const button = document.getElementById('myButton')
 const text = document.getElementById('text')
+// const form = document.getElementById('myForm')
+const p = document.getElementById('para')
+const input = document.getElementById('myinput')
 
-button.addEventListener('click', () => {
-    text.innerHTML = 'hello by new text!'
-    document.body.style.backgroundColor = 'yellow'
+// button.addEventListener('click', () => {
+//     text.innerHTML = 'hello by new text!'
+//     document.body.style.backgroundColor = 'yellow'
+// })
+
+// button.addEventListener('mouseover', () => {
+//     text.innerHTML = 'hello by new text!'
+// })
+
+// text.addEventListener('mouseover', () => {
+//     document.body.style.color = 'blue'
+// })
+
+// text.addEventListener('mouseout', (event) => {
+//     document.body.style.color = 'black'
+// })
+
+// form.addEventListener('click', (e) => {
+//     e.preventDefault()
+//     console.log(e.target)
+// })
+
+input.addEventListener('input', (e) => {
+    p.textContent = input.value
+    e.preventDefault()
 })
